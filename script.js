548feb2a079314a4ecaef679f010fc242b88558b
Aryan Purwar <aryanpurwar0@outlook.com>
2024-06-11 02:42:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function me() {
+    document.querySelector('.info').style.display = 'block';
+    document.querySelector('.Education').style.display = 'none';
+    document.querySelector('.Skills').style.display = 'none';
+    document.querySelector('.experinces').style.display = 'none';
+  }
+  
+  function edu() {
+    document.querySelector('.info').style.display = 'none';
+    document.querySelector('.Education').style.display = 'block';
+    document.querySelector('.Skills').style.display = 'none';
+    document.querySelector('.experinces').style.display = 'none';
+  }
+  
+  function skills() {
+    document.querySelector('.info').style.display = 'none';
+    document.querySelector('.Education').style.display = 'none';
+    document.querySelector('.Skills').style.display = 'block';
+    document.querySelector('.experinces').style.display = 'none';
+  }
+  
+  function exp() {
+    document.querySelector('.info').style.display = 'none';
+    document.querySelector('.Education').style.display = 'none';
+    document.querySelector('.Skills').style.display = 'none';
+    document.querySelector('.experinces').style.display = 'block';
+  }
